ab7a81e567caceac482ec779f1e2aed6b5afbea0
Wilka Hudson <wilka.hudson@gmail.com>
2020-09-05 13:15:43 +0100

    Init project with ReactiveUI

---
+++ SpellyPerfApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace SpellyPerfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainWindowViewModel();
+            
+            
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.Bind(ViewModel, vm => vm.InputText, view => view.InputBox.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel, vm => vm.OutputText, view => view.OutputBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
---
+++ SpellyPerfApp/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace SpellyPerfApp
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+            this.WhenAnyValue(me => me.InputText)
+                .ToPropertyEx(this, me => me.OutputText);
+        }
+
+        [Reactive]
+        public string InputText { get; set; }
+
+        [ObservableAsProperty]
+        public string OutputText { get; }
+    }
+}
